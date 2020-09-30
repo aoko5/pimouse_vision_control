d6afe523dbae87bab0737330d0871ee6ce58105a
aoko5 <aoko111aoko@gmail.com>
2020-09-30 17:06:47 +0000

    Add packages for travis

--- test/travis_package_make.bash
+++ test/travis_package_make.bash
@@ -1,9 +1,15 @@
 #!/bin/bash -xve
 
+sudo apt-get update
+sudo apt-get install opencv-data libopencd-dev python-opencv
+
+sudo apt-get install ros-kinetic-cv-bridge
+sudo apt-get install ros-kinetic-cv-camera
+
 rsync -av ./ ~/catkin_ws/src/pimouse_vision_control/
 
 cd ~/catkin_ws/src/
 git clone --depth=1 https://github.com/aoko5/pimouse_ros.git
 
 cd ~/catkin_ws
-catkin_makecatkin_make
+catkin_make
